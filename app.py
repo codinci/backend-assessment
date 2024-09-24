1d141545d229e7b3a3a8b4e21362c86854c03e0b
codinci <dkibau@gmail.com>
2024-09-24 07:50:24 +0300

    Added SQLite database on user authentication

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 
 import json
+import secrets
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 
 ENV_FILE = find_dotenv()
@@ -14,6 +16,23 @@ if ENV_FILE:
 
 app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///customer_orders.db'
+
+db = SQLAlchemy(app)
+
+# Define the User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(100))
+
+    def __init__(self, email, name):
+        self.email = email
+        self.name = name
+
+#OAuth Configuration
 
 oauth = OAuth(app)
 
@@ -30,14 +49,39 @@ oauth.register(
 
 @app.route("/login")
 def login():
+    # Generate a random nonce and store it in the session
+    nonce = secrets.token_urlsafe(16)
+    session['nonce'] = nonce
     return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("callback", _external=True)
+        redirect_uri=url_for("callback", _external=True),
+        nonce=nonce
     )
 
 
 @app.route("/callback", methods=["GET", "POST"])
 def callback():
     token = oauth.auth0.authorize_access_token()
+
+ 	# Retrieve the nonce from the session
+    nonce = session.get('nonce')
+
+    if not nonce:
+        return "Error: Nonce not found in session", 400
+
+    user_info = oauth.auth0.parse_id_token(token, nonce=nonce)
+
+    email = user_info.get("email")
+    name = user_info.get("name")
+
+    # Check if user exists
+    existing_user = User.query.filter_by(email=email).first()
+
+    if not existing_user:
+        new_user = User(email=email, name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+	#Store user session
     session["user"] = token
     return redirect("/")
 
@@ -64,4 +108,6 @@ def home():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=env.get("PORT", 5000))
